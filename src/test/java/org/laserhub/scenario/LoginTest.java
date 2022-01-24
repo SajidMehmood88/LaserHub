d0e2c72f74cc6a4b69b6860616e05397a7407df1
sajid.mehmood <sajid.mehmood@idealo.de>
2022-01-24 21:43:32 +0100

    Login test class

---
+++ src/test/java/org/laserhub/scenario/LoginTest.java
@@ -0,0 +1,52 @@
+package org.laserhub.scenario;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.laserhub.pageobject.ChooseAccountPage;
+import org.laserhub.pageobject.LoginPage;
+import org.laserhub.resources.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LoginTest extends BaseTest {
+
+    public static Logger log = LogManager.getLogger(BaseTest.class.getName());
+
+    private LoginPage loginPage;
+    private ChooseAccountPage chooseAccountPage;
+
+
+    @BeforeClass
+    public void initialize() {
+        loginPage = new LoginPage(driver);
+        chooseAccountPage = new ChooseAccountPage(driver);
+        driver.get("https://app.laserhub.com/login");
+    }
+
+    @Test(dataProvider = "validCredentials")
+    public void validLogin(String email, String password) {
+        loginPage.enterEmail(email);
+        loginPage.enterPassword(password);
+        loginPage.clickOnSubmitButton();
+        chooseAccountPage.isCompanyChoosePageDisplayed();
+        String expectedUrl = "https://app.laserhub.com/register/company-choose";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+        log.info("Successfully logs in");
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.close();
+    }
+
+    @DataProvider
+    public Object[][] validCredentials() {
+        Object[][] account = new Object[1][2];
+        account[0][0] = "e.ioannidis+testing_worktask@laserhub.com";
+        account[0][1] = "l0vet3sting@";
+        return account;
+    }
+
+
+}
